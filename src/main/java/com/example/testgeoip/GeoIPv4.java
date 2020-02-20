693861cdc634bbad0436e82d66b77e8324203ab2
Dilipsaikumar <dilip.sai@practo.com>
2020-02-20 20:53:15 +0530

    added

---
+++ src/main/java/com/example/testgeoip/GeoIPv4.java
@@ -0,0 +1,39 @@
+package com.example.testgeoip;
+
+import com.maxmind.geoip.LookupService;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class GeoIPv4 {
+
+    private static LookupService lookUp;
+
+    static {
+        try {
+            lookUp = new LookupService(
+                    GeoIPv4.class.getResource("/GeoLiteCity.dat").getFile(),
+                    LookupService.GEOIP_MEMORY_CACHE);
+
+            System.out.println("GeoIP Database loaded: " + lookUp.getDatabaseInfo());
+        } catch (IOException e) {
+            System.out.println("Could not load geo ip database: " + e.getMessage());
+        }
+    }
+
+//    public static GeoLocation getLocation(String ipAddress) {
+//        return GeoLocation.map(lookUp.getLocation(ipAddress));
+//    }
+    
+    public static String getLocation(String ipAddress) {
+        return lookUp.getLocation(ipAddress).countryName;
+    }
+
+    public static GeoLocation getLocation(long ipAddress){
+        return GeoLocation.map(lookUp.getLocation(ipAddress));
+    }
+
+    public static GeoLocation getLocation(InetAddress ipAddress){
+        return GeoLocation.map(lookUp.getLocation(ipAddress));
+    }
+}
